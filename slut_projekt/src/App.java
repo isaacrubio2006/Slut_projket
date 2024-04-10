5bffb7e73f699aa5a8413e2e2f7843e9d2e6545f
isaac rubio <isaacamaru.rubio@elev.ga.ntig.se>
2024-04-10 10:23:42 +0200

    slut_projekt

---
+++ slut_projekt/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(" gokväll du vill boka flyg");
+        System.out.println(" 1. slump tal\n 2.\n 3. avsluta\n");
+    }
+}
