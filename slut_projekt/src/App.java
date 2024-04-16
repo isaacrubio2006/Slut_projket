fae5f463e4bcc8c55a6556c0fe5fa98f337bae7a
isaac rubio <isaacamaru.rubio@elev.ga.ntig.se>
2024-04-16 20:06:07 +0200

    fortsatt på kod

--- slut_projekt/src/App.java
+++ slut_projekt/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(" gokväll du vill boka flyg");
         System.out.println(" 1. slump tal\n 2.\n 3. avsluta\n");
-        int Apris= 299;
+        double Apris= 249.99;
+        double bpris= 149.99;
     }
 }
