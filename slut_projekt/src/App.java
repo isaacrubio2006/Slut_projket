e2c290259d9396c33eb4875523119d2b8051bf1d
isaac rubio <isaacamaru.rubio@elev.ga.ntig.se>
2024-04-16 20:06:29 +0200

    fortsatt

--- slut_projekt/src/App.java
+++ slut_projekt/src/App.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(" gokväll du vill boka flyg");
-        System.out.println(" 1. slump tal\n 2.\n 3. avsluta\n");
+        System.out.println(" gokväll du vill boka buss");
+      String[] bokning = new String[20];
+      int[] personnmr = new int[20];
+      int val = 0;
         double Apris= 249.99;
         double bpris= 149.99;
+        while (val != 4) {
+            Scanner tangentbord = new Scanner(System.in);
+            System.out.println(" 1. lägg till bokning \n 2. Visa innehåll\n 3. Beräkna vinst sålda biljetter \n 4. avsluta \n ");
+             val =tangentbord.nextInt();
+
+             if (val==1) {
+                System.out.println("skriv namn och personnummer: ");
+                String boka = tangentbord.nextLine();
+                if (bokning.length == 20) {
+                    System.out.println("Det finns bara 20 platser det är upptaget");
+                }
+                else {
+                        
+                }
+             }
+        }
     }
 }
